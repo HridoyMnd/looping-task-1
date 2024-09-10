afec7ff755e15ee423a5b8594ea4e8609151f611
Hridoy Mnd <hridoymnd10@gmail.com>
2024-09-10 17:53:49 +0600

    looping task 1 done

---
+++ script.js
@@ -0,0 +1,11 @@
+//looping task 1
+
+ const fruits = ['orange', 'yellow', 'green', 'blue', 'red'];
+
+    let reverse = [];
+
+ for(let i = fruits.length - 1; i >= 0; i--){
+    reverse.push(fruits[i]);
+ }
+ 
+ console.log(reverse);
